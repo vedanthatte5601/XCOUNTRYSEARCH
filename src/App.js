d8a851171538a170a2b1fb361cdb3f5e64333ceb
vedanthatte5601 <vedanthatte5601@gmail.com>
2024-03-29 23:43:46 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(response => response.json())
+      .then(data => {
+        setCountries(data);
+        setSearchResults(data);
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    setSearchTerm(searchTerm);
+    const filteredCountries = countries.filter(country =>
+      country.name.common.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(filteredCountries);
+  };
+
+  const imageStyle = {
+    width: "100px",
+    height: "100px",
+  };
+
+  const containerStyle = {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+  };
+
+  const cardStyle = {
+    width: "200px",
+    border: "1px solid #ccc",
+    borderRadius: "10px",
+    margin: "10px",
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  
+  const inputStyle ={
+    position: "sticky",
+    width : "500px",
+    height: "30px",
+    border: "10px"
+  }
+  const containerStyle2 = {
+    width : "100vh"
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {containerStyle2}>
+      <input
+        type="text"
+        placeholder="Search for a country..."
+        value={searchTerm}
+        onChange={handleSearch}
+        style = {inputStyle}
+      />
+      <div style={containerStyle}>
+      {searchResults.map((country) => {
+        return (
+          <div className = "countryCard" key={country.cca3} style={cardStyle}>
+            <img
+              src={country.flags.png}
+              alt={`flat of ${country.name.common}`}
+              style={imageStyle}
+            />
+            <h2>{country.name.common}</h2>
+          </div>
+        );
+      })}
+    </div>
     </div>
   );
 }
